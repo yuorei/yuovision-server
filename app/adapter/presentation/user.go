464f9b56372610d1a40163960b90d6223538a389
yuorei <108039575+yuorei@users.noreply.github.com>
2024-04-05 14:32:46 +0900

    user関連

--- app/adapter/presentation/user.go
+++ app/adapter/presentation/user.go
@@ -1,6 +1,8 @@
 package presentation
 
 import (
+	"context"
+
 	"github.com/yuorei/video-server/app/application"
 	"github.com/yuorei/video-server/yuovision-proto/go/video/video_grpc"
 )
@@ -14,4 +16,31 @@ func NewUserService(app *application.Application) *UserService {
 type UserService struct {
 	video_grpc.UnimplementedUserServiceServer
 	usecase *application.UseCase
+}
+
+func (s *UserService) User(ctx context.Context, input *video_grpc.UserID) (*video_grpc.UserPayload, error) {
+	user, err := s.usecase.UserInputPort.GetUser(ctx, input.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &video_grpc.UserPayload{
+		Id:                  user.ID,
+		Name:                user.Name,
+		ProfileImageUrl:     user.ProfileImageURL,
+		SubscribeChannelIds: user.Subscribechannelids,
+	}, nil
+}
+
+func (s *UserService) RegisterUser(ctx context.Context, input *video_grpc.UserInput) (*video_grpc.UserPayload, error) {
+	user, err := s.usecase.UserInputPort.RegisterUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &video_grpc.UserPayload{
+		Id:                  user.ID,
+		Name:                user.Name,
+		ProfileImageUrl:     user.ProfileImageURL,
+		SubscribeChannelIds: user.Subscribechannelids,
+	}, nil
 }
