ab0dd4226ebe677cd3350297255f238c76884f09
yuorei <108039575+yuorei@users.noreply.github.com>
2024-04-05 14:35:40 +0900

    ビデオサービスにVideoメソッドを追加

--- app/adapter/presentation/video.go
+++ app/adapter/presentation/video.go
@@ -1,8 +1,11 @@
 package presentation
 
 import (
+	"context"
+
 	"github.com/yuorei/video-server/app/application"
 	"github.com/yuorei/video-server/yuovision-proto/go/video/video_grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func NewVideoService(app *application.Application) *VideoService {
@@ -14,4 +17,22 @@ func NewVideoService(app *application.Application) *VideoService {
 type VideoService struct {
 	video_grpc.UnimplementedVideoServiceServer
 	usecase *application.UseCase
+}
+
+func (s *VideoService) Video(ctx context.Context, id *video_grpc.VideoID) (*video_grpc.VideoPayload, error) {
+	video, err := s.usecase.GetVideo(ctx, id.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &video_grpc.VideoPayload{
+		Id:                video.ID,
+		VideoUrl:          video.VideoURL,
+		Title:             video.Title,
+		ThumbnailImageUrl: video.ThumbnailImageURL,
+		Description:       *video.Description,
+		CreatedAt:         timestamppb.New(video.CreatedAt),
+		UpdatedAt:         timestamppb.New(video.UpdatedAt),
+		UserId:            video.UploaderID,
+	}, nil
 }
