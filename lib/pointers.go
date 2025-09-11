502683262792adf933240047826f62069021e70a
yuorei <108039575+yuorei@users.noreply.github.com>
2025-09-12 01:30:20 +0900

    Add ConvertStringSliceToPointerSlice function to convert []string to []*string

--- lib/pointers.go
+++ lib/pointers.go
@@ -4,3 +4,12 @@ package lib
 func StringPointer(s string) *string {
 	return &s
 }
+
+// Helper function to convert []string to []*string
+func ConvertStringSliceToPointerSlice(slice []string) []*string {
+	result := make([]*string, len(slice))
+	for i, str := range slice {
+		result[i] = &str
+	}
+	return result
+}
